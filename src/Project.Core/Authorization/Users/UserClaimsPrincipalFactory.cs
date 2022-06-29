bcc321aa4114034b2badc39a78db0c4ae1166df5
imanBahrampour <iman.bahrampour@gmail.com>
2022-06-29 11:44:48 -0700

    'create_custom_claim'

--- src/Project.Core/Authorization/Users/UserClaimsPrincipalFactory.cs
+++ src/Project.Core/Authorization/Users/UserClaimsPrincipalFactory.cs
@@ -3,11 +3,16 @@ using Microsoft.Extensions.Options;
 using Abp.Authorization;
 using Project.Authorization.Roles;
 using Abp.Domain.Uow;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace Project.Authorization.Users
 {
     public class UserClaimsPrincipalFactory : AbpUserClaimsPrincipalFactory<User, Role>
     {
+        public UserManager _userManager { get; set; }
+
         public UserClaimsPrincipalFactory(
             UserManager userManager,
             RoleManager roleManager,
@@ -19,6 +24,17 @@ namespace Project.Authorization.Users
                   optionsAccessor,
                   unitOfWorkManager)
         {
+            this._userManager = userManager;
+        }
+        public override async Task<ClaimsPrincipal> CreateAsync(User user)
+        {
+
+            var claim = await base.CreateAsync(user);
+            claim.Identities.First().AddClaim(new Claim("Name", user.Name));
+            claim.Identities.First().AddClaim(new Claim("UserName", user.UserName));
+            var result = _userManager.GetOrganizationUnits(user).Select(o => o.Id);
+            claim.Identities.First().AddClaim(new Claim("OUIds", string.Join('-', _userManager.GetOrganizationUnits(user).Select(o => o.Id))));
+            return claim;
         }
     }
 }
